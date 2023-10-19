188ed08d138514ba85dd9bd30a96b6ed2ae6458c
Rashida Strozier <105885953+RStrozier@users.noreply.github.com>
2023-10-19 11:03:56 -0400

    uploaded documents for pull

---
+++ script.js
@@ -0,0 +1,16 @@
+var cards = document.querySelectorAll(".card");
+
+var observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+    })
+},
+    {
+        rootMargin: "-100px",
+    }
+)
+
+cards.forEach(card => {
+    observer.observe(card)
+})
+
